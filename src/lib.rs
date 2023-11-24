911369b377dc03e1139880a31b8cdb036c92cef6
Adoni5 <roryjmunro1@gmail.com>
2023-11-24 15:02:39 +0000

    Fixed maths errors lol

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 //! A python module for quickly and efficiently calculating a dynamic CNV profile from contigs continaed in a  BAM file.
 
 use fnv::FnvHashMap;
-use indicatif::ParallelProgressIterator;
+use indicatif::{ParallelProgressIterator, ProgressBar, ProgressStyle};
 use pyo3::prelude::*;
 use rayon::prelude::*;
 use rust_htslib::bam::{self, FetchDefinition, Read, Record};
@@ -45,13 +45,13 @@ impl BamFile {
 }
 
 /// Calculate median value
-fn median(numbers: &mut [u16]) -> Option<f32> {
+fn median(numbers: &mut [u16]) -> Option<f64> {
     numbers.par_sort();
     let mid = numbers.len() / 2;
     if numbers.len() % 2 == 0 {
-        Some((numbers[mid - 1] as f32 + numbers[mid] as f32) / 2.0)
+        Some((numbers[mid - 1] as f64 + numbers[mid] as f64) / 2.0)
     } else {
-        Some(numbers[mid] as f32)
+        Some(numbers[mid] as f64)
     }
 }
 
@@ -60,34 +60,36 @@ pub fn calculate_cnv(
     genome_length: usize,
     number_reads: usize,
     bins: FnvHashMap<String, Vec<u16>>,
-) -> FnvHashMap<String, Vec<u16>> {
+) -> (FnvHashMap<String, Vec<f64>>, usize) {
     // bin_size = int(genome_length / (total_map_starts / reads_per_bin))
     let bin_width =
         (genome_length as f64 / (number_reads as f64 / READS_PER_BIN as f64).ceil()) as usize;
-    let mut all_values: Vec<u16> = bins.values().flat_map(|v| v.iter().copied()).collect();
+    let mut all_values: Vec<u16> = bins
+        .values()
+        .flat_map(|v| v.iter().copied())
+        .collect::<Vec<_>>()
+        .chunks(bin_width / BIN_SIZE)
+        .map(|chunk| chunk.iter().sum::<u16>())
+        .collect::<Vec<_>>();
 
-    let median_value: usize = median(&mut all_values).unwrap().round() as usize;
+    let median_value: f64 = median(&mut all_values).unwrap().round();
+    println!("Median value: {}", median_value);
 
-    let new_map: FnvHashMap<String, Vec<u16>> = bins
+    let new_map: FnvHashMap<String, Vec<f64>> = bins
         .into_par_iter()
-        .progress_count(23)
         .map(|(k, v)| {
             let sums = v
-                .chunks(bin_width)
+                .chunks(bin_width / BIN_SIZE)
                 .map(|chunk| {
-                    chunk
-                        .iter()
-                        .sum::<u16>()
-                        .checked_div(median_value as u16)
-                        .unwrap_or(0_u16)
-                        .checked_mul(PLOIDY)
-                        .unwrap_or(0)
+                    std::convert::Into::<f64>::into(chunk.iter().sum::<u16>())
+                        / (median_value as f64)
+                        * (PLOIDY as f64)
                 })
-                .collect::<Vec<u16>>();
+                .collect::<Vec<f64>>();
             (k, sums)
         })
         .collect();
-    new_map
+    (new_map, bin_width)
 }
 
 /// Results struct for python
@@ -96,7 +98,7 @@ pub fn calculate_cnv(
 pub struct CnvResult {
     /// The CNV per contig
     #[pyo3(get)]
-    pub cnv: FnvHashMap<String, Vec<u16>>,
+    pub cnv: FnvHashMap<String, Vec<f64>>,
     /// Bin width
     #[pyo3(get)]
     pub bin_width: usize,
@@ -117,10 +119,16 @@ fn iterate_bam_file(bam_file_path: PathBuf, threads: Option<usize>) -> PyResult<
         .expect("Error setting threads");
     let header = bam_reader.header().to_owned();
     let num_targets = header.target_count();
-    // calculate whole genome length
-    let genome_length: u64 = (0..num_targets)
-        .map(|tid| header.target_len(tid).unwrap())
-        .sum();
+
+    // Get total number of reads
+
+    let (genome_length, total_mapped, total_unmapped): (u64, u64, u64) =
+        bam_reader.index_stats().unwrap().into_iter().fold(
+            (0, 0, 0),
+            |(acc2, acc3, acc4), (_, second, third, fourth)| {
+                (acc2 + second, acc3 + third, acc4 + fourth)
+            },
+        );
 
     let mut results: FnvHashMap<String, Vec<u16>> = FnvHashMap::default();
     // Prepare all the CNV bins
@@ -139,6 +147,14 @@ fn iterate_bam_file(bam_file_path: PathBuf, threads: Option<usize>) -> PyResult<
     let mut number_reads: usize = 0;
     bam_reader.fetch(FetchDefinition::All).unwrap();
     let mut record = Record::new();
+    let bar = ProgressBar::new(total_mapped + total_unmapped);
+    bar.set_style(
+        ProgressStyle::with_template(
+            "[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}",
+        )
+        .unwrap()
+        .progress_chars("##-"),
+    );
     while let Some(result) = bam_reader.read(&mut record) {
         match result {
             Ok(_) => {
@@ -147,34 +163,19 @@ fn iterate_bam_file(bam_file_path: PathBuf, threads: Option<usize>) -> PyResult<
                         std::str::from_utf8(header.tid2name(record.tid() as u32)).unwrap();
 
                     results.get_mut(target_name).unwrap()[record.pos() as usize / BIN_SIZE] += 1;
-                    number_reads += 1;
                 }
+                number_reads += 1;
             }
             Err(_) => panic!("BAM parsing failed..."),
         }
-        if number_reads % 100000 == 0 {
-            println!("Number of reads read: {}", number_reads);
-        }
+        bar.inc(1);
     }
     println!("Number of reads: {}", number_reads);
-    let cnv_profile = calculate_cnv(genome_length as usize, number_reads, results);
+    let (cnv_profile, bin_width) = calculate_cnv(genome_length as usize, number_reads, results);
     let result = CnvResult {
         cnv: cnv_profile,
-        bin_width: BIN_SIZE,
+        bin_width,
     };
-    // let counts = bam_reader
-    //     .rc_records()
-    //     .map(|x| x.expect("Failure parsing Bam file")) // Converts Iterator<Result<Record, Error>> to Iterator<Record>, ignoring errors // Keep only first occurrences
-    //     .inspect(|x| println!("{:#?}", x.flags()))
-    //     .map(|record| record.pos() as usize / BIN_SIZE)
-    //     .inspect(|x| println!("{:#?}", x)) // Map each record to its bin
-    //     .fold(HashMap::new(), |mut acc, bin| {
-    //         // Count occurrences in each bin
-    //         *acc.entry(bin).or_insert(0) += 1;
-    //         acc
-    //     });
-    // println!("{:#?}", counts);
-    // println!("{:#?}", cnv_profile);
     Ok(result)
 }
 
