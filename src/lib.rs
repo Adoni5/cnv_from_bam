e72a2da3312de6c163c2d6257f5d76a94b6536f6
Adoni5 <roryjmunro1@gmail.com>
2023-11-27 16:16:14 +0000

    Filter for map quality Add docstrings Filter for secondary and multiple mappings

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 //! A python module for quickly and efficiently calculating a dynamic CNV profile from contigs continaed in a  BAM file.
 
 use fnv::FnvHashMap;
-use indicatif::{ParallelProgressIterator, ProgressBar, ProgressStyle};
+use indicatif::{ProgressBar, ProgressStyle};
 use pyo3::prelude::*;
 use rayon::prelude::*;
 use rust_htslib::bam::{self, FetchDefinition, Read, Record};
@@ -44,8 +44,42 @@ impl BamFile {
     }
 }
 
-/// Calculate median value
+/// Calculates the median of a slice of `u16` numbers.
+///
+/// The function first sorts the given slice in place and then computes the median.
+/// If the slice length is even, the median is the average of the two middle values.
+/// If the slice length is odd, the median is the middle value.
+///
+/// # Arguments
+///
+/// * `numbers` - A mutable reference to a slice of `u16` numbers.
+///
+/// # Returns
+///
+/// An `Option<f64>` containing the median. If the slice is empty, it returns `None`.
+///
+/// # Examples
+///
+/// ```rust,ignore
+/// # fn main() {
+/// // Even number of elements
+/// let mut even_numbers = [2, 3, 1, 4];
+/// assert_eq!(median::median(&mut even_numbers), Some(2.5));
+///
+/// // Odd number of elements
+/// let mut odd_numbers = [1, 2, 3, 4, 5];
+/// assert_eq!(median::median(&mut odd_numbers), Some(3.0));
+///
+/// // Empty slice
+/// let mut no_numbers: [u16; 0] = [];
+/// assert_eq!(median::median(&mut no_numbers), None);
+/// # }
+/// ```
+///
 fn median(numbers: &mut [u16]) -> Option<f64> {
+    if numbers.is_empty() {
+        return None;
+    }
     numbers.par_sort();
     let mid = numbers.len() / 2;
     if numbers.len() % 2 == 0 {
@@ -55,7 +89,45 @@ fn median(numbers: &mut [u16]) -> Option<f64> {
     }
 }
 
-/// Calculate dynamically binned CNV for each contig
+/// Calculates Copy Number Variations (CNVs) for each contig in a genome.
+///
+/// This function dynamically bins the genome based on the total number of reads and
+/// the length of the genome. It then calculates the CNV value for each bin in each contig.
+///
+/// # Arguments
+///
+/// * `genome_length` - A `usize` representing the total length of the genome.
+/// * `number_reads` - A `usize` representing the total number of reads.
+/// * `bins` - A `FnvHashMap<String, Vec<u16>>` where keys are contig names and values
+///            are vectors containing read counts for each bin in that contig.
+///
+/// # Returns
+///
+/// Returns a tuple containing:
+/// - A `FnvHashMap<String, Vec<f64>>` where each key is a contig name and the value
+///   is a vector of CNV values for each bin in that contig.
+/// - A `usize` representing the width of each bin.
+///
+/// # Examples
+///
+/// ```rust
+/// use fnv::FnvHashMap;
+/// use cnv_from_bam::calculate_cnv;  // Replace with the actual crate name
+///
+/// // Example data
+/// let genome_length = 3000;
+/// let number_reads = 1500;
+/// let mut bins = FnvHashMap::default();
+/// bins.insert("contig1".to_string(), vec![100, 200, 150, 250]);
+/// bins.insert("contig2".to_string(), vec![300, 400, 350, 450]);
+///
+/// let (cnv_map, bin_width) = calculate_cnv(genome_length, number_reads, bins);
+///
+/// // Use cnv_map and bin_width as needed
+/// // e.g., print the CNV values for contig1
+/// println!("Contig1 CNVs: {:?}", cnv_map.get("contig1"));
+/// ```
+///
 pub fn calculate_cnv(
     genome_length: usize,
     number_reads: usize,
@@ -64,11 +136,13 @@ pub fn calculate_cnv(
     // bin_size = int(genome_length / (total_map_starts / reads_per_bin))
     let bin_width =
         (genome_length as f64 / (number_reads as f64 / READS_PER_BIN as f64).ceil()) as usize;
+    let chunk_size = bin_width / BIN_SIZE;
+    let chunk_size = if chunk_size == 0 { 1 } else { chunk_size };
     let mut all_values: Vec<u16> = bins
         .values()
         .flat_map(|v| v.iter().copied())
         .collect::<Vec<_>>()
-        .chunks(bin_width / BIN_SIZE)
+        .chunks(chunk_size)
         .map(|chunk| chunk.iter().sum::<u16>())
         .collect::<Vec<_>>();
 
@@ -79,7 +153,7 @@ pub fn calculate_cnv(
         .into_par_iter()
         .map(|(k, v)| {
             let sums = v
-                .chunks(bin_width / BIN_SIZE)
+                .chunks(chunk_size)
                 .map(|chunk| {
                     std::convert::Into::<f64>::into(chunk.iter().sum::<u16>())
                         / (median_value as f64)
@@ -102,12 +176,56 @@ pub struct CnvResult {
     /// Bin width
     #[pyo3(get)]
     pub bin_width: usize,
+    /// Genome length
+    #[pyo3(get)]
+    pub genome_length: usize,
 }
 
-/// Iterate a bam file
+/// Iterates over a BAM file to compute Copy Number Variation (CNV) profiles for each contig.
+///
+/// This function reads through a BAM file, filters the alignments based on mapping quality,
+/// and calculates CNV profiles using dynamically sized bins. It uses multithreading for improved performance.
+///
+/// # Arguments
+///
+/// * `bam_file_path` - A `PathBuf` specifying the path to the BAM file.
+/// * `threads` - An `Option<usize>` specifying the number of threads to use for reading the BAM file.
+///              If `None`, the number of CPUs is used.
+/// * `mapq_filter` - An `Option<u8>` specifying the minimum mapping quality to consider.
+///                   Alignments with a mapping quality below this value are ignored. Default is 60.
+///
+/// # Returns
+///
+/// A `PyResult<CnvResult>` containing the CNV profiles and additional information about the analysis.
+///
+/// # Examples
+///
+/// ```rust,ignore
+/// use std::path::PathBuf;
+/// use cnv_from_bam::iterate_bam_file;  // Replace with the actual crate name
+///
+/// // Example usage of iterate_bam_file
+/// let bam_path = PathBuf::from("path/to/bam/file.bam");
+/// let result = iterate_bam_file(bam_path, Some(4), Some(60));
+/// match result {
+///     Ok(cnv_result) => {
+///         // Process the CNV results
+///         println!("Bin width: {}", cnv_result.bin_width);
+///     },
+///     Err(e) => println!("Error: {:?}", e),
+/// }
+/// ```
+///
+/// Note: This function is designed to be used in a Python context through PyO3 bindings.
 #[pyfunction]
-fn iterate_bam_file(bam_file_path: PathBuf, threads: Option<usize>) -> PyResult<CnvResult> {
+fn iterate_bam_file(
+    bam_file_path: PathBuf,
+    threads: Option<usize>,
+    mapq_filter: Option<u8>,
+) -> PyResult<CnvResult> {
+    let mapq_filter = mapq_filter.unwrap_or(60);
     let bam_file = BamFile::new(bam_file_path);
+    // Open bam file
     let mut bam_reader = bam::IndexedReader::from_path(&bam_file.path).unwrap_or_else(|_| {
         panic!(
             "Error creating bam reader with file {}",
@@ -120,8 +238,7 @@ fn iterate_bam_file(bam_file_path: PathBuf, threads: Option<usize>) -> PyResult<
     let header = bam_reader.header().to_owned();
     let num_targets = header.target_count();
 
-    // Get total number of reads
-
+    // Get total number of reads, mapped and unmapped
     let (genome_length, total_mapped, total_unmapped): (u64, u64, u64) =
         bam_reader.index_stats().unwrap().into_iter().fold(
             (0, 0, 0),
@@ -143,10 +260,10 @@ fn iterate_bam_file(bam_file_path: PathBuf, threads: Option<usize>) -> PyResult<
         println!("Target length: {}", target_length);
     }
     println!("Genome length: {}", genome_length);
-    // number of reads in the bam file
-    let mut number_reads: usize = 0;
+    // number of reads in the bam file that match our criteria
+    let mut valid_number_reads: usize = 0;
     bam_reader.fetch(FetchDefinition::All).unwrap();
-    let mut record = Record::new();
+    // Setup progress bar
     let bar = ProgressBar::new(total_mapped + total_unmapped);
     bar.set_style(
         ProgressStyle::with_template(
@@ -155,26 +272,37 @@ fn iterate_bam_file(bam_file_path: PathBuf, threads: Option<usize>) -> PyResult<
         .unwrap()
         .progress_chars("##-"),
     );
+    // Allocate record
+    let mut record = Record::new();
     while let Some(result) = bam_reader.read(&mut record) {
         match result {
             Ok(_) => {
-                if !record.is_unmapped() {
+                if !record.is_unmapped()
+                    & (record.mapq() > mapq_filter)
+                    & !record.is_secondary()
+                    & !record.is_supplementary()
+                {
                     let target_name =
                         std::str::from_utf8(header.tid2name(record.tid() as u32)).unwrap();
 
                     results.get_mut(target_name).unwrap()[record.pos() as usize / BIN_SIZE] += 1;
                 }
-                number_reads += 1;
+                valid_number_reads += 1;
             }
             Err(_) => panic!("BAM parsing failed..."),
         }
         bar.inc(1);
     }
-    println!("Number of reads: {}", number_reads);
-    let (cnv_profile, bin_width) = calculate_cnv(genome_length as usize, number_reads, results);
+    println!(
+        "Number of reads matching criteria mapq >{} and aligned: {}",
+        mapq_filter, valid_number_reads
+    );
+    let (cnv_profile, bin_width) =
+        calculate_cnv(genome_length as usize, valid_number_reads, results);
     let result = CnvResult {
         cnv: cnv_profile,
         bin_width,
+        genome_length: genome_length as usize,
     };
     Ok(result)
 }
@@ -194,9 +322,44 @@ mod tests {
     fn test_iterate_bam_file() {
         // Call the function with the temporary BAM file
         let bam_file_path = PathBuf::from("test/test.bam");
-        let result = iterate_bam_file(bam_file_path.clone(), None).expect("Function failed");
+        let result =
+            iterate_bam_file(bam_file_path.clone(), None, Some(60)).expect("Function failed");
         println!("{:#?}", result);
         // Assert that the function returns a CnvBins instance
         // assert_eq!(result.bins, vec![0, 0]);
+    }
+
+    #[test]
+    fn test_median_even_number_of_elements() {
+        let mut numbers = [2, 3, 1, 4];
+        let result = median(&mut numbers).unwrap();
+        assert_eq!(result, 2.5);
+    }
+
+    #[test]
+    fn test_median_odd_number_of_elements() {
+        let mut numbers = [1, 2, 3, 4, 5];
+        let result = median(&mut numbers).unwrap();
+        assert_eq!(result, 3.0);
+    }
+
+    #[test]
+    fn test_median_empty_slice() {
+        let mut numbers: [u16; 0] = [];
+        assert!(median(&mut numbers).is_none());
+    }
+
+    #[test]
+    fn test_median_with_repeated_elements() {
+        let mut numbers = [1, 1, 1, 1];
+        let result = median(&mut numbers).unwrap();
+        assert_eq!(result, 1.0);
+    }
+
+    #[test]
+    fn test_median_with_single_element() {
+        let mut numbers = [42];
+        let result = median(&mut numbers).unwrap();
+        assert_eq!(result, 42.0);
     }
 }
